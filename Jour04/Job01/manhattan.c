1a50b0ffcaa2956343db2a7bfb43715b6f73b404
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2023-11-30 10:55:31 +0100

    Update manhattan.c

--- Jour04/Job01/manhattan.c
+++ Jour04/Job01/manhattan.c
@@ -9,14 +9,14 @@ int manhattan(point a, point b) {
     return res;
 }
 
-int main() {
-    point p1;
-    p1.x = 5;
-    p1.y = 2;
+// int main() {
+//     point p1;
+//     p1.x = 5;
+//     p1.y = 2;
 
-    point p2;
-    p2.x = 1;
-    p2.y = 2;
+//     point p2;
+//     p2.x = 1;
+//     p2.y = 2;
 
-    printf("%d\n", manhattan(p1, p2));
-}
+//     printf("%d\n", manhattan(p1, p2));
+// }
